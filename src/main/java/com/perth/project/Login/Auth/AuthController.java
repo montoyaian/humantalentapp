5d2ba7a74a1b85620ac57d57386e3dfe784160f6
montoya <montoyita41@hotmail.com>
2024-12-13 00:05:52 -0500

    Cross origin

--- src/main/java/com/perth/project/Login/Auth/AuthController.java
+++ src/main/java/com/perth/project/Login/Auth/AuthController.java
@@ -6,11 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 public class AuthController {
     private final Authservice authService;
--- src/main/java/com/perth/project/Login/Protected/ProtectedEndPoints.java
+++ src/main/java/com/perth/project/Login/Protected/ProtectedEndPoints.java
@@ -4,10 +4,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 public class ProtectedEndPoints {
 
